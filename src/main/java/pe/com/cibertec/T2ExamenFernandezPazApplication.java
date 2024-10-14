c11e70a1f09741778208f646a06a5bc952d0434a
Alessander9 <Alessanderneedsponsors@gmail.com>
2024-10-13 19:11:12 -0500

    Estructura inicial del examen T2

---
+++ src/main/java/pe/com/cibertec/T2ExamenFernandezPazApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2ExamenFernandezPazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2ExamenFernandezPazApplication.class, args);
+	}
+
+}
